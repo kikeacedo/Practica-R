ea5202500a27fafc23e0d69a6e21d10e0597a126
Quique <enrique.acedo@WKSALMPU1141.u-tad.es>
2015-11-10 10:01:53 +0100

    1.0

--- practica_2.r
+++ practica_2.r
@@ -4,6 +4,32 @@
 # Data Mining
 
 # Leemos los datos
-social_data <- read.csv("")
+social_data <- read.csv("https://raw.githubusercontent.com/kikeacedo/Practica-R/master/social_network.csv")
 
-str(social_data)
+# Ver los datos
+# str(social_data)
+
+# Filtrar datos fuera de rango de edad [13, 20)
+social_data$age <- ifelse(social_data$age >= 13 & 
+                          social_data$age < 20,
+                          social_data$age, NA )
+
+
+# 
+social_data$female <- ifelse(social_data$gender == "F" &
+                            !is.na(social_data$gender),
+                            1, 0)
+
+social_data$no_gender <- ifelse(is.na(social_data$gender), 1, 0)
+
+mean(social_data$age, na.rm = TRUE)
+
+aggregate(data = social_data, age ~ gradyear, mean, na.rm = TRUE)
+
+
+ave_age <- ave(social_data$age, social_data$gradyear, 
+               FUN = function(x) mean(x, na.rm = TRUE))
+
+social_data$age <- ifelse(is.na(social_data$age), ave_age, social_data$age)
+
+summary(social_data$age)
